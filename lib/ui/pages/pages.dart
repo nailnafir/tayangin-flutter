94474690f394c08e61c33267962ecf6ce1f17a66
Nailul Firdaus <nail.nafir@gmail.com>
2021-01-27 14:47:27 +0700

    membuat page bloc

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -4,6 +4,6 @@ import 'package:provider/provider.dart';
 import 'package:tayangin_flutter/services/services.dart';
 
 part 'main_page.dart';
-part 'sign_in_page.dart';
+part '../../bloc/sign_in_page.dart';
 
 part 'wrapper.dart';
