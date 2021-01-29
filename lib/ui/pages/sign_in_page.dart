39a0f2e7f6b3354be0be6d2fc71db008a5cf8667
Nailul Firdaus <nail.nafir@gmail.com>
2021-01-30 01:06:05 +0700

    update splash page dan sign in page

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -19,134 +19,140 @@ class _SignInPageState extends State<SignInPage> {
         .bloc<ThemeBloc>()
         .add(ChangeTheme(ThemeData().copyWith(primaryColor: mainColorYellow)));
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-          child: ListView(
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(height: 50),
-                  SizedBox(
-                      height: 80,
-                      width: 80,
-                      child: Image.asset("assets/logo_tayangin_blue.png")),
-                  Container(
-                    margin: EdgeInsets.only(top: 50, bottom: 30),
-                    child: Text(
-                      "Selamat datang,\nTayangquwh!",
-                      style: blackTextFont.copyWith(fontSize: 20),
-                    ),
-                  ),
-                  TextField(
-                    onChanged: (text) {
-                      setState(() {
-                        isEmailValid = EmailValidator.validate(text);
-                      });
-                    },
-                    controller: emailController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        labelText: "Email Address"),
-                  ),
-                  SizedBox(height: 20),
-                  TextField(
-                    onChanged: (text) {
-                      setState(() {
-                        isPasswordValid = text.length >= 6;
-                      });
-                    },
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        labelText: "Password"),
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    children: <Widget>[
-                      Text("Lupa password? ",
-                          style: grayTextFont.copyWith(
-                              fontWeight: FontWeight.w400)),
-                      Text("Reset disini",
-                          style: yellowTextFont.copyWith(
-                              fontWeight: FontWeight.w400)),
-                    ],
-                  ),
-                  Center(
-                    child: Container(
-                      width: 250,
-                      height: 50,
-                      margin: EdgeInsets.only(top: 50, bottom: 20),
-                      child: isSigningIn
-                          ? SpinKitThreeBounce(color: mainColorBlue)
-                          : FloatingActionButton(
-                              child: Text("Masuk",
-                                  style: isEmailValid && isPasswordValid
-                                      ? whiteTextFont.copyWith(fontSize: 16)
-                                      : grayTextFont.copyWith(fontSize: 16)),
-                              backgroundColor: isEmailValid && isPasswordValid
-                                  ? mainColorBlue
-                                  : accentColorLightGray,
-                              elevation: 2,
-                              onPressed: isEmailValid && isPasswordValid
-                                  ? () async {
-                                      setState(() {
-                                        isSigningIn = true;
-                                      });
-                                      SignInSignUpResult result =
-                                          await AuthServices.signIn(
-                                              emailController.text,
-                                              passwordController.text);
-
-                                      if (result.user == null) {
-                                        setState(() {
-                                          isSigningIn = false;
-                                        });
-
-                                        Flushbar(
-                                          duration: Duration(seconds: 5),
-                                          flushbarPosition:
-                                              FlushbarPosition.TOP,
-                                          backgroundColor: accentColorRed,
-                                          message: result.message,
-                                        ).show(context);
-                                      }
-                                    }
-                                  : null,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8))),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "Belum punya akun? ",
-                        style:
-                            grayTextFont.copyWith(fontWeight: FontWeight.w400),
+    return WillPopScope(
+      onWillPop: () {
+        context.bloc<PageBloc>().add(GoToSplashPage());
+        return;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+            padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+            child: ListView(
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 50),
+                    SizedBox(
+                        height: 80,
+                        width: 80,
+                        child: Image.asset("assets/logo_tayangin_blue.png")),
+                    Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 30),
+                      child: Text(
+                        "Selamat datang,\nTayangquwh!",
+                        style: blackTextFont.copyWith(fontSize: 20),
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          context.bloc<PageBloc>().add(GoToLoginPage());
-                        },
-                        child: Text(
-                          "Buat disini",
-                          style: yellowTextFont.copyWith(
+                    ),
+                    TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          isEmailValid = EmailValidator.validate(text);
+                        });
+                      },
+                      controller: emailController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          labelText: "Email Address"),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          isPasswordValid = text.length >= 6;
+                        });
+                      },
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          labelText: "Password"),
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      children: <Widget>[
+                        Text("Lupa password? ",
+                            style: grayTextFont.copyWith(
+                                fontWeight: FontWeight.w400)),
+                        Text("Reset disini",
+                            style: yellowTextFont.copyWith(
+                                fontWeight: FontWeight.w400)),
+                      ],
+                    ),
+                    Center(
+                      child: Container(
+                        width: 250,
+                        height: 50,
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: isSigningIn
+                            ? SpinKitThreeBounce(color: mainColorBlue)
+                            : FloatingActionButton(
+                                child: Text("Masuk",
+                                    style: isEmailValid && isPasswordValid
+                                        ? whiteTextFont.copyWith(fontSize: 16)
+                                        : grayTextFont.copyWith(fontSize: 16)),
+                                backgroundColor: isEmailValid && isPasswordValid
+                                    ? mainColorBlue
+                                    : accentColorLightGray,
+                                elevation: 2,
+                                onPressed: isEmailValid && isPasswordValid
+                                    ? () async {
+                                        setState(() {
+                                          isSigningIn = true;
+                                        });
+                                        SignInSignUpResult result =
+                                            await AuthServices.signIn(
+                                                emailController.text,
+                                                passwordController.text);
+
+                                        if (result.user == null) {
+                                          setState(() {
+                                            isSigningIn = false;
+                                          });
+
+                                          Flushbar(
+                                            duration: Duration(seconds: 5),
+                                            flushbarPosition:
+                                                FlushbarPosition.TOP,
+                                            backgroundColor: accentColorRed,
+                                            message: result.message,
+                                          ).show(context);
+                                        }
+                                      }
+                                    : null,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8))),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Belum punya akun? ",
+                          style: grayTextFont.copyWith(
                               fontWeight: FontWeight.w400),
                         ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ],
-          )),
+                        GestureDetector(
+                          onTap: () {
+                            context.bloc<PageBloc>().add(GoToLoginPage());
+                          },
+                          child: Text(
+                            "Buat disini",
+                            style: yellowTextFont.copyWith(
+                                fontWeight: FontWeight.w400),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -3,79 +3,72 @@ part of 'pages.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () {
-        context.bloc<PageBloc>().add(GoToSplashPage());
-        return;
-      },
-      child: Scaffold(
-        backgroundColor: mainColorBlue,
-        body: Container(
-            padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  height: 140,
-                  width: 140,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/logo_tayangin_yellow.png"),
-                    ),
+    return Scaffold(
+      backgroundColor: mainColorBlue,
+      body: Container(
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 140,
+                width: 140,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/logo_tayangin_yellow.png"),
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.only(top: 80, bottom: 18),
-                  child: Text(
-                    "Hiburan Seru",
-                    style: whiteTextFont.copyWith(
-                        fontSize: 20, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                Text(
-                  "Temukan hiburanmu dengan \nmenonton banyak film tanpa ribet",
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 80, bottom: 18),
+                child: Text(
+                  "Hiburan Seru",
                   style: whiteTextFont.copyWith(
-                      fontSize: 16, fontWeight: FontWeight.w300),
-                  textAlign: TextAlign.center,
+                      fontSize: 20, fontWeight: FontWeight.w500),
                 ),
-                Container(
-                  width: 250,
-                  height: 50,
-                  margin: EdgeInsets.only(top: 50, bottom: 20),
-                  child: RaisedButton(
-                    child: Text("Mulai",
-                        style: blackTextFont.copyWith(fontSize: 16)),
-                    color: mainColorYellow,
-                    onPressed: () {},
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
+              ),
+              Text(
+                "Temukan hiburanmu dengan \nmenonton banyak film tanpa ribet",
+                style: whiteTextFont.copyWith(
+                    fontSize: 16, fontWeight: FontWeight.w300),
+                textAlign: TextAlign.center,
+              ),
+              Container(
+                width: 250,
+                height: 50,
+                margin: EdgeInsets.only(top: 50, bottom: 20),
+                child: RaisedButton(
+                  child: Text("Mulai",
+                      style: blackTextFont.copyWith(fontSize: 16)),
+                  color: mainColorYellow,
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Sudah punya akun? ",
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Sudah punya akun? ",
+                    style: whiteTextFont.copyWith(fontWeight: FontWeight.w400),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      context.bloc<PageBloc>().add(GoToLoginPage());
+                    },
+                    child: Text(
+                      "Masuk disini",
                       style:
-                          whiteTextFont.copyWith(fontWeight: FontWeight.w400),
+                          yellowTextFont.copyWith(fontWeight: FontWeight.w400),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        context.bloc<PageBloc>().add(GoToLoginPage());
-                      },
-                      child: Text(
-                        "Masuk disini",
-                        style: yellowTextFont.copyWith(
-                            fontWeight: FontWeight.w400),
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            )),
-      ),
+                  )
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
