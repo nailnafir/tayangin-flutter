0b6faf26d2295cea7cacf867b1ca7ea7defc04de
Nailul Firdaus <nail.nafir@gmail.com>
2021-01-29 07:49:50 +0700

    mengganti font style

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -11,14 +11,13 @@ Color accentColorLightGray = Color(0xFFE3E3E3);
 Color accentColorRed = Color(0xFFF12153);
 Color accentColorGreen = Color(0xFF00AB1B);
 
-TextStyle blackTextFont = GoogleFonts.montserrat()
+TextStyle blackTextFont = GoogleFonts.poppins()
     .copyWith(color: Colors.black, fontWeight: FontWeight.w500);
-TextStyle whiteTextFont = GoogleFonts.montserrat()
+TextStyle whiteTextFont = GoogleFonts.poppins()
     .copyWith(color: Colors.white, fontWeight: FontWeight.w500);
 
 TextStyle yellowNumberFont =
-    GoogleFonts.montserrat().copyWith(color: mainColorYellow);
-TextStyle redNumberFont =
-    GoogleFonts.montserrat().copyWith(color: accentColorRed);
+    GoogleFonts.poppins().copyWith(color: mainColorYellow);
+TextStyle redNumberFont = GoogleFonts.poppins().copyWith(color: accentColorRed);
 TextStyle greenNumberFont =
-    GoogleFonts.montserrat().copyWith(color: accentColorGreen);
+    GoogleFonts.poppins().copyWith(color: accentColorGreen);
