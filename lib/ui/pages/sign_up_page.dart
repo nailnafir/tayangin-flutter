e389f121d52f30c95c4ebb79084d004cbc775e37
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-04 17:54:33 +0700

    deklarasi registration data pada bloc

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -1,1 +1,65 @@
 part of 'pages.dart';
+
+class SignUpPage extends StatefulWidget {
+  final RegistrationData registrationData;
+  SignUpPage(this.registrationData);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController retypePasswordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    nameController.text = widget.registrationData.name;
+    emailController.text = widget.registrationData.email;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        context.bloc<PageBloc>().add(GoToSplashPage());
+        return;
+      },
+      child: Scaffold(
+        body: Container(
+          color: Colors.white,
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          child: ListView(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 16, bottom: 30),
+                    height: 24,
+                    child: Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: GestureDetector(
+                            onTap: () {
+                              context.bloc<PageBloc>().add(GoToSplashPage());
+                            },
+                            child: Icon(Icons.arrow_back, color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
