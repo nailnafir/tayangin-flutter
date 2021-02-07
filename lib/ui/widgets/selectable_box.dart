214b41cd258b528e45e4d50cd27ddc05a38c97d3
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-07 21:31:18 +0700

    membuat widget selectable box untuk banyak page

---
+++ lib/ui/widgets/selectable_box.dart
@@ -0,0 +1,55 @@
+part of 'widgets.dart';
+
+class SelectableBox extends StatelessWidget {
+  final bool isSelected;
+  final bool isEnabled;
+  final double width;
+  final double height;
+  final String text;
+  final Function onTap;
+  final TextStyle textStyle;
+
+  SelectableBox(this.text,
+      {this.isSelected = false,
+      this.isEnabled = true,
+      this.width = 144,
+      this.height = 60,
+      this.onTap,
+      this.textStyle});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap();
+        }
+      },
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: (!isEnabled)
+              ? accentColorLightGray
+              : isSelected
+                  ? mainColorYellow
+                  : Colors.transparent,
+          border: Border.all(
+              color: (!isEnabled)
+                  ? accentColorLightGray
+                  : isSelected
+                      ? Colors.transparent
+                      : accentColorLightGray),
+        ),
+        child: Center(
+          child: Text(
+            text ?? "none",
+            style: (textStyle ?? blackTextFont)
+                .copyWith(fontSize: 14, fontWeight: FontWeight.w400),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/widgets/widgets.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:tayangin_flutter/shared/shared.dart';
+
+part 'selectable_box.dart';
