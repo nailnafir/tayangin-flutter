e1962c1a12cc4c61f358a7d3341bb2fa87e31836
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-12 15:36:53 +0700

    menambahkan update data pada user event

--- lib/bloc/user_event.dart
+++ lib/bloc/user_event.dart
@@ -19,3 +19,13 @@ class SignOut extends UserEvent {
   @override
   List<Object> get props => [];
 }
+
+class UpdateData extends UserEvent {
+  final String name;
+  final String profileImage;
+
+  UpdateData({this.name, this.profileImage});
+
+  @override
+  List<Object> get props => [name, profileImage];
+}
--- lib/ui/pages/movie_page.dart
+++ lib/ui/pages/movie_page.dart
@@ -18,6 +18,13 @@ class MoviePage extends StatelessWidget {
           child: BlocBuilder<UserBloc, UserState>(
             builder: (_, userState) {
               if (userState is UserLoaded) {
+                if (imageFileToUpload != null) {
+                  uploadImage(imageFileToUpload).then((downloadURL) {
+                    imageFileToUpload = null;
+                    context.bloc<UserBloc>().add(event);
+                  });
+                }
+
                 return Row(
                   children: <Widget>[
                     Column(
