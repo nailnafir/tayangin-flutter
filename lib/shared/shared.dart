73d4bed816ec4be8011e925270117e4aaca6354d
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-12 15:16:06 +0700

    membuat shared methods dan memindahkan method image picker dari pages

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tayangin_flutter/bloc/blocs.dart';
 
 part 'shared_value.dart';
+part 'shared_methods.dart';
 part 'theme.dart';
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -8,7 +8,6 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart' as provider;
 import 'package:google_fonts/google_fonts.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
@@ -27,8 +26,3 @@ part 'preference_page.dart';
 part 'account_confirmation_page.dart';
 part 'movie_page.dart';
 part 'wrapper.dart';
-
-Future<File> getImage() async {
-  var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-  return image;
-}
