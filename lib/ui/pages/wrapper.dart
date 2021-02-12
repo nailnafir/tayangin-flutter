2b9ee60da048d56ea139b0fb84ce17e4ded9ab57
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-12 09:46:30 +0700

    menambahkan account confirmation page pada wrapper

--- lib/ui/pages/wrapper.dart
+++ lib/ui/pages/wrapper.dart
@@ -36,6 +36,9 @@ class Wrapper extends StatelessWidget {
                     ? SignUpPage(pageState.registrationData)
                     : (pageState is OnPreferencePage)
                         ? PreferencePage(pageState.registrationData)
-                        : MainPage());
+                        : (pageState is OnAccountConfirmationPage)
+                            ? AccountConfirmationPage(
+                                pageState.registrationData)
+                            : MainPage());
   }
 }
