1c372d8138a60a3bd496eb7d4bf0ce0a3ea0077a
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-12 14:42:38 +0700

    deklarasi file image to upload pada shared value

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
--- lib/shared/shared_value.dart
+++ lib/shared/shared_value.dart
@@ -4,3 +4,4 @@ String apiKey = "c8d8956be56524af43299f98c8a2677c";
 String imageBaseURL = "https://image.tmdb.org/t/p/";
 
 PageEvent prevPageEvent;
+File imageFileToUpload;
