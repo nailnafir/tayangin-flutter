ecf573df386f49c660aa76b87c3925fc78301bca
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-13 15:35:14 +0700

    menambahkan fetch movie pada movie bloc main

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
               BlocProvider(create: (_) => PageBloc()),
               BlocProvider(create: (_) => UserBloc()),
               BlocProvider(create: (_) => ThemeBloc()),
-              BlocProvider(create: (_) => MovieBloc()),
+              BlocProvider(create: (_) => MovieBloc()..add(FetchMovies())),
             ],
             child: BlocBuilder<ThemeBloc, ThemeState>(
               builder: (_, themeState) => MaterialApp(
