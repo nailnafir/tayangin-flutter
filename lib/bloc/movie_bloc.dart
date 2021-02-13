b4d26424ca43f34b5426b1ed0d4ea982218c5397
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-13 15:28:40 +0700

    membuat movie bloc

---
+++ lib/bloc/movie_bloc.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:tayangin_flutter/models/models.dart';
+import 'package:tayangin_flutter/services/services.dart';
+
+part 'movie_event.dart';
+part 'movie_state.dart';
+
+class MovieBloc extends Bloc<MovieEvent, MovieState> {
+  @override
+  MovieState get initialState => MovieInitial();
+
+  @override
+  Stream<MovieState> mapEventToState(
+    MovieEvent event,
+  ) async* {
+    if (event is FetchMovies) {
+      List<Movie> movies = await MovieServices.getMovies(1);
+    }
+  }
+}
