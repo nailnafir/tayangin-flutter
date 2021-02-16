9466b98f9e91770314b523091b9313173ecc1d4a
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-16 13:59:57 +0700

    menambahkan movie detail page pada page bloc

---
+++ lib/ui/pages/movie_detail_page.dart
@@ -0,0 +1,13 @@
+part of 'pages.dart';
+
+class MovieDetail extends StatelessWidget {
+  final Movie movie;
+  MovieDetail(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -26,3 +26,4 @@ part 'preference_page.dart';
 part 'account_confirmation_page.dart';
 part 'movie_page.dart';
 part 'wrapper.dart';
+part 'movie_detail_page.dart';
