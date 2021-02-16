dce021ce0b24705ee5d14faeefa3dfe712f72752
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-16 15:12:03 +0700

    perbaikan movie card, movie page, dan membuat movie detail page

--- lib/ui/pages/movie_detail_page.dart
+++ lib/ui/pages/movie_detail_page.dart
@@ -6,8 +6,45 @@ class MovieDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    MovieDetail movieDetail;
+    List<Credit> credits;
+
+    return WillPopScope(
+      onWillPop: () async {
+        context.bloc<PageBloc>().add(GoToMainPage());
+        return;
+      },
+      child: Scaffold(
+        body: FutureBuilder(
+            future: MovieServices.getDetails(movie),
+            builder: (_, snapshot) {
+              if (snapshot.hasData) {
+                movieDetail = snapshot.data;
+                return FutureBuilder(
+                    future: MovieServices.getCredits(movie.id),
+                    builder: (_, snapshot) {
+                      if (snapshot.hasData) {
+                        credits = snapshot.data;
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(movieDetail.title),
+                            Text(movieDetail.genresAndLanguage),
+                            Column(
+                              children:
+                                  credits.map((e) => Text(e.name)).toList(),
+                            )
+                          ],
+                        );
+                      } else {
+                        return SizedBox();
+                      }
+                    });
+              } else {
+                return SizedBox();
+              }
+            }),
+      ),
     );
   }
 }
--- lib/ui/widgets/movie_card.dart
+++ lib/ui/widgets/movie_card.dart
@@ -8,43 +8,50 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 140,
-      width: 200,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        image: DecorationImage(
-            image: NetworkImage(imageBaseURL + "w780" + movie.backdropPath),
-            fit: BoxFit.cover),
-      ),
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap();
+        }
+      },
       child: Container(
         height: 140,
         width: 200,
-        padding: EdgeInsets.all(10),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8),
-          gradient: LinearGradient(
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            colors: [
-              Colors.black.withOpacity(0.5),
-              Colors.black.withOpacity(0)
+          image: DecorationImage(
+              image: NetworkImage(imageBaseURL + "w780" + movie.backdropPath),
+              fit: BoxFit.cover),
+        ),
+        child: Container(
+          height: 140,
+          width: 200,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              colors: [
+                Colors.black.withOpacity(0.5),
+                Colors.black.withOpacity(0)
+              ],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                movie.title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: whiteTextFont.copyWith(
+                    fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+              RatingStars(voteAverage: movie.voteAverage)
             ],
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              movie.title,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: whiteTextFont.copyWith(
-                  fontSize: 14, fontWeight: FontWeight.w500),
-            ),
-            RatingStars(voteAverage: movie.voteAverage)
-          ],
         ),
       ),
     );
