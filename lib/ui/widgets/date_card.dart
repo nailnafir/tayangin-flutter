d59aeb71019c97c2453a06216aad5709e59a2949
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-18 21:42:39 +0700

    membuat date card widget

---
+++ lib/ui/widgets/date_card.dart
@@ -0,0 +1,56 @@
+part of 'widgets.dart';
+
+class DateCard extends StatelessWidget {
+  final bool isSelected;
+  final double width;
+  final double height;
+  final DateTime date;
+  final Function onTap;
+
+  DateCard(this.date,
+      {this.isSelected = false,
+      this.width = 80,
+      this.height = 100,
+      this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap();
+        }
+      },
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: isSelected ? mainColorYellow : Colors.transparent,
+          border: Border.all(
+              color: isSelected ? Colors.transparent : accentColorLightGray),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              date.dayName,
+              style: blackTextFont.copyWith(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              (date.day.toString() + date.month.toString()),
+              style: blackTextFont.copyWith(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:tayangin_flutter/models/models.dart';
 import 'package:tayangin_flutter/shared/shared.dart';
+import 'package:tayangin_flutter/extensions/extensions.dart';
 
 part 'selectable_box.dart';
 part 'movie_card.dart';
@@ -11,3 +12,4 @@ part 'browse_button.dart';
 part 'coming_soon_card.dart';
 part 'promo_card.dart';
 part 'credit_card.dart';
+part 'date_card.dart';
