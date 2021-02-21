76d4bb383ab4e221447b2ce9adc97dfaa8694080
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-21 08:01:27 +0700

    menyelesaikan select schedule page

--- lib/ui/pages/select_schedule_page.dart
+++ lib/ui/pages/select_schedule_page.dart
@@ -11,6 +11,9 @@ class SelectSchedulePage extends StatefulWidget {
 class _SelectSchedulePageState extends State<SelectSchedulePage> {
   List<DateTime> dates;
   DateTime selectedDate;
+  int selectedTime;
+  Theater selectedTheater;
+  bool isValid = false;
 
   @override
   void initState() {
@@ -67,8 +70,8 @@ class _SelectSchedulePageState extends State<SelectSchedulePage> {
                 ),
                 //NOTE: CHOOSE DATE
                 Container(
-                  margin:
-                      EdgeInsets.fromLTRB(defaultMargin, 30, defaultMargin, 12),
+                  margin: EdgeInsets.fromLTRB(
+                      defaultMargin, defaultMargin, defaultMargin, 12),
                   child: Text(
                     "Pilih Tanggal",
                     style: blackTextFont.copyWith(
@@ -78,34 +81,107 @@ class _SelectSchedulePageState extends State<SelectSchedulePage> {
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(bottom: 30),
+                  margin: EdgeInsets.only(bottom: defaultMargin),
                   height: 100,
                   child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: dates.length,
-                      itemBuilder: (_, index) => Container(
-                            margin: EdgeInsets.only(
-                              left: (index == 0) ? defaultMargin : 0,
-                              right: (index < dates.length - 1)
-                                  ? 18
-                                  : defaultMargin,
-                            ),
-                            child: DateCard(
-                              dates[index],
-                              isSelected: selectedDate == dates[index],
-                              onTap: () {
-                                setState(() {
-                                  selectedDate = dates[index];
-                                });
-                              },
-                            ),
-                          )),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: dates.length,
+                    itemBuilder: (_, index) => Container(
+                      margin: EdgeInsets.only(
+                        left: (index == 0) ? defaultMargin : 0,
+                        right: (index < dates.length - 1) ? 18 : defaultMargin,
+                      ),
+                      child: DateCard(
+                        dates[index],
+                        isSelected: selectedDate == dates[index],
+                        onTap: () {
+                          setState(() {
+                            selectedDate = dates[index];
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                //NOTE: CHOOSE TIME & LOCATION
+                generateTimeTable(),
+                SizedBox(height: 10),
+                //NOTE: BUTTON NEXT
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    height: 50,
+                    width: 250,
+                    margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+                    child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        color: (isValid) ? mainColorBlue : accentColorLightGray,
+                        elevation: 2,
+                        child: Text(
+                          "Lanjutkan",
+                          style: isValid
+                              ? whiteTextFont.copyWith(
+                                  fontSize: 16, fontWeight: FontWeight.w500)
+                              : grayTextFont.copyWith(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                        onPressed: () {}),
+                  ),
                 )
               ],
             )
           ],
         ),
       ),
+    );
+  }
+
+  Column generateTimeTable() {
+    List<int> schedule = List.generate(7, (index) => 10 + index * 2);
+    List<Widget> widgets = [];
+
+    for (var theater in dummyTheaters) {
+      widgets.add(Container(
+        margin: EdgeInsets.fromLTRB(defaultMargin, 0, defaultMargin, 12),
+        child: Text(theater.name,
+            style: blackTextFont.copyWith(
+                fontSize: 18, fontWeight: FontWeight.w500)),
+      ));
+
+      widgets.add(Container(
+        height: 50,
+        margin: EdgeInsets.only(bottom: defaultMargin),
+        child: ListView.builder(
+            itemCount: schedule.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (_, index) => Container(
+                  margin: EdgeInsets.only(
+                    left: (index == 0) ? defaultMargin : 0,
+                    right: (index == schedule.length - 1) ? defaultMargin : 18,
+                  ),
+                  child: SelectableBox(
+                    "${schedule[index]}:00",
+                    height: 50,
+                    isSelected: selectedTheater == theater &&
+                        selectedTime == schedule[index],
+                    isEnabled: schedule[index] > DateTime.now().hour ||
+                        selectedDate.day != DateTime.now().day,
+                    onTap: () {
+                      setState(() {
+                        selectedTheater = theater;
+                        selectedTime = schedule[index];
+                        isValid = true;
+                      });
+                    },
+                  ),
+                )),
+      ));
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: widgets,
     );
   }
 }
