01ba2a0e5f4282cf24cbacbe188fa9418224e271
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-24 15:43:39 +0700

    membuat movie description pada checkout page

--- lib/ui/pages/checkout_page.dart
+++ lib/ui/pages/checkout_page.dart
@@ -11,6 +11,121 @@ class CheckoutPage extends StatefulWidget {
 class _CheckoutPageState extends State<CheckoutPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return WillPopScope(
+        onWillPop: () async {
+          context.bloc<PageBloc>().add(GoToSelectSeatPage(widget.ticket));
+          return;
+        },
+        child: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              Container(color: mainColorBlue),
+              SafeArea(child: Container(color: Colors.white)),
+              ListView(
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      //NOTE: BACK BUTTON
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            margin:
+                                EdgeInsets.only(top: 16, left: defaultMargin),
+                            padding: EdgeInsets.all(1),
+                            child: GestureDetector(
+                                onTap: () {
+                                  context
+                                      .bloc<PageBloc>()
+                                      .add(GoToSelectSeatPage(widget.ticket));
+                                },
+                                child: Icon(Icons.arrow_back,
+                                    color: Colors.black)),
+                          )
+                        ],
+                      ),
+                      BlocBuilder<UserBloc, UserState>(builder: (_, userState) {
+                        UserApp user = (userState as UserLoaded).user;
+                        return Column(
+                          children: <Widget>[
+                            //NOTE: PAGE TITLE
+                            Center(
+                              child: Container(
+                                margin: EdgeInsets.only(top: 16, bottom: 30),
+                                child: Text(
+                                  "Detail Pembelian",
+                                  style: blackTextFont.copyWith(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            ),
+                            //NOTE: MOVIE DESCRIPTION
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  width: 80,
+                                  height: 100,
+                                  margin: EdgeInsets.only(
+                                      left: defaultMargin, right: 18),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      image: DecorationImage(
+                                          image: NetworkImage(imageBaseURL +
+                                              "w342" +
+                                              widget.ticket.movieDetail
+                                                  .posterPath),
+                                          fit: BoxFit.cover)),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Container(
+                                      width: MediaQuery.of(context).size.width -
+                                          2 * defaultMargin -
+                                          80 -
+                                          18,
+                                      child: Text(
+                                        widget.ticket.movieDetail.title,
+                                        maxLines: 2,
+                                        overflow: TextOverflow.clip,
+                                        style: blackTextFont.copyWith(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width -
+                                          2 * defaultMargin -
+                                          80 -
+                                          18,
+                                      margin: EdgeInsets.symmetric(vertical: 4),
+                                      child: Text(
+                                        widget.ticket.movieDetail
+                                            .genresAndLanguage,
+                                        style: grayTextFont.copyWith(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w400),
+                                      ),
+                                    ),
+                                    RatingStars(
+                                      voteAverage:
+                                          widget.ticket.movieDetail.voteAverage,
+                                      color: accentColorDarkGray,
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        );
+                      }),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
