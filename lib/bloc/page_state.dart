617576f2409da9867ed3fb7bde582d3bd8ecc47f
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-24 14:34:39 +0700

    menambahkan checkout page pada page bloc

--- lib/bloc/page_state.dart
+++ lib/bloc/page_state.dart
@@ -74,3 +74,11 @@ class OnSelectSeatPage extends PageState {
   @override
   List<Object> get props => [ticket];
 }
+
+class OnCheckoutPage extends PageState {
+  final Ticket ticket;
+  OnCheckoutPage(this.ticket);
+
+  @override
+  List<Object> get props => [ticket];
+}
