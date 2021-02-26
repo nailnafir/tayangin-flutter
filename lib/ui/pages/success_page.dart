ca00f0ef9f81721493c2b98c95fb04601c403244
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-26 14:47:07 +0700

    menambahkan ui style success buy dan success top up

--- lib/ui/pages/success_page.dart
+++ lib/ui/pages/success_page.dart
@@ -22,6 +22,8 @@ class SuccessPage extends StatelessWidget {
             builder: (_, snapshot) =>
                 (snapshot.connectionState == ConnectionState.done)
                     ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
                           Container(
                             width: 250,
@@ -29,9 +31,9 @@ class SuccessPage extends StatelessWidget {
                             margin: EdgeInsets.only(bottom: 80),
                             decoration: BoxDecoration(
                                 image: DecorationImage(
-                                    image: provider.Svg(ticket == null
-                                        ? "assets/illustration_success_top_up.svg"
-                                        : "assets/illustration_success_buy.svg"))),
+                                    image: AssetImage(ticket == null
+                                        ? "assets/illustration_success_top_up.png"
+                                        : "assets/illustration_success_buy.png"))),
                           ),
                           Text(
                             ticket == null
@@ -48,7 +50,41 @@ class SuccessPage extends StatelessWidget {
                             style: grayTextFont.copyWith(
                                 fontSize: 14, fontWeight: FontWeight.w400),
                           ),
-                          Container()
+                          Container(
+                            width: 250,
+                            height: 50,
+                            margin: EdgeInsets.only(top: 50, bottom: 20),
+                            child: RaisedButton(
+                              elevation: 2,
+                              color: mainColorBlue,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Text(
+                                ticket == null ? "Dompet Saya" : "Tiket Saya",
+                                style: whiteTextFont.copyWith(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Mau nonton film lagi? ",
+                                  style: grayTextFont.copyWith(
+                                      fontWeight: FontWeight.w400)),
+                              GestureDetector(
+                                onTap: () {
+                                  context.bloc<PageBloc>().add(GoToMainPage());
+                                },
+                                child: Text("Yuk lah!",
+                                    style: yellowTextFont.copyWith(
+                                        fontWeight: FontWeight.w400)),
+                              ),
+                            ],
+                          )
                         ],
                       )
                     : Center(
