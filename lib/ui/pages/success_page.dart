b3e0d7c2d4fa97589b57f337e5cdd375bc65b5b1
Nailul Firdaus <nail.nafir@gmail.com>
2021-02-26 14:20:40 +0700

    perbaikan asset image

--- lib/ui/pages/success_page.dart
+++ lib/ui/pages/success_page.dart
@@ -13,6 +13,7 @@ class SuccessPage extends StatelessWidget {
         return;
       },
       child: Scaffold(
+        backgroundColor: Colors.white,
         body: FutureBuilder(
             // jika ada tiketnya, berarti lagi beli tiket, kalo gak ada ya topup
             future: ticket != null
@@ -20,10 +21,35 @@ class SuccessPage extends StatelessWidget {
                 : processingTopUp(),
             builder: (_, snapshot) =>
                 (snapshot.connectionState == ConnectionState.done)
-                    ? Center(
-                        child: RaisedButton(onPressed: () {
-                          context.bloc<PageBloc>().add(GoToMainPage());
-                        }),
+                    ? Column(
+                        children: <Widget>[
+                          Container(
+                            width: 250,
+                            height: 250,
+                            margin: EdgeInsets.only(bottom: 80),
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: provider.Svg(ticket == null
+                                        ? "assets/illustration_success_top_up.svg"
+                                        : "assets/illustration_success_buy.svg"))),
+                          ),
+                          Text(
+                            ticket == null
+                                ? "Hmmm, Syukurlah!"
+                                : "Yuhuuu, Nonton Film!",
+                            style: blackTextFont.copyWith(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          ),
+                          SizedBox(height: 18),
+                          Text(
+                            ticket == null
+                                ? "Sekarang saldo kamu\nbertambah banyak"
+                                : "Kamu telah berhasil\nmembeli tiket bioskop",
+                            style: grayTextFont.copyWith(
+                                fontSize: 14, fontWeight: FontWeight.w400),
+                          ),
+                          Container()
+                        ],
                       )
                     : Center(
                         child: SpinKitFadingCircle(
