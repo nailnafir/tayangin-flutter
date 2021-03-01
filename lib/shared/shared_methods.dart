c6e270218ec987e51f0a7cae5181ac5a9b268057
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-01 13:33:55 +0700

    menambahkan generate dash divider pada shared method

--- lib/shared/shared_methods.dart
+++ lib/shared/shared_methods.dart
@@ -21,3 +21,20 @@ Future<String> uploadImage(File image) async {
   // setelah mendapat snapshot hasil dari task, kembalikan download url nya
   return await snapshot.ref.getDownloadURL();
 }
+
+Widget generateDashedDivider(double width) {
+  int n = width ~/ 5;
+  return Row(
+    children: List.generate(
+        n,
+        (index) => (index % 2 == 0)
+            ? Container(
+                height: 2,
+                width: width / n,
+                color: accentColorLightGray,
+              )
+            : SizedBox(
+                width: width / n,
+              )),
+  );
+}
--- lib/ui/pages/ticket_detail_page.dart
+++ lib/ui/pages/ticket_detail_page.dart
@@ -19,51 +19,236 @@ class TicketDetailPage extends StatelessWidget {
           children: <Widget>[
             Container(color: mainColorBlue),
             SafeArea(child: Container(color: Colors.white)),
-            ListView(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    //NOTE: HEADER
-                    Container(
-                      margin: EdgeInsets.only(top: 16),
-                      child: Stack(
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(left: defaultMargin),
-                            padding: EdgeInsets.all(1),
-                            alignment: Alignment.centerLeft,
-                            child: GestureDetector(
-                              onTap: () {
-                                context.bloc<PageBloc>().add(GoToMainPage(
-                                    bottomNavBarIndex: 1,
-                                    isExpired:
-                                        ticket.time.isBefore(DateTime.now())));
-                              },
-                              child:
-                                  Icon(Icons.arrow_back, color: Colors.black),
-                            ),
-                          ),
-                          Center(
-                            child: Text(
-                              "Tiket Detail",
-                              style: blackTextFont.copyWith(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+              child: ListView(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      //NOTE: HEADER
+                      Container(
+                        margin: EdgeInsets.only(top: 16),
+                        child: Stack(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(1),
+                              alignment: Alignment.centerLeft,
+                              child: GestureDetector(
+                                onTap: () {
+                                  context.bloc<PageBloc>().add(GoToMainPage(
+                                      bottomNavBarIndex: 1,
+                                      isExpired: ticket.time
+                                          .isBefore(DateTime.now())));
+                                },
+                                child:
+                                    Icon(Icons.arrow_back, color: Colors.black),
                               ),
-                              textAlign: TextAlign.center,
                             ),
-                          ),
-                        ],
+                            Center(
+                              child: Text(
+                                "Tiket Detail",
+                                style: blackTextFont.copyWith(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    //NOTE: TICKET
-                  ],
-                )
-              ],
+                      //NOTE: TICKET
+                      Container(
+                        margin: EdgeInsets.only(top: 30),
+                        height: 180,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(8),
+                              topRight: Radius.circular(8)),
+                          image: DecorationImage(
+                            image: NetworkImage(imageBaseURL +
+                                "w500" +
+                                ticket.movieDetail.backdropPath),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      ClipPath(
+                        clipper: TicketTopClipper(),
+                        child: Container(
+                          width: double.infinity,
+                          color: accentColorYellow,
+                          padding: EdgeInsets.fromLTRB(18, 18, 18, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                ticket.movieDetail.title,
+                                maxLines: 2,
+                                overflow: TextOverflow.clip,
+                                style: blackTextFont.copyWith(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                ticket.movieDetail.genresAndLanguage,
+                                style: grayTextFont.copyWith(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(height: 18),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(
+                                    "Lokasi",
+                                    style: grayTextFont.copyWith(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.45,
+                                    child: Text(
+                                      ticket.theater.name,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.clip,
+                                      textAlign: TextAlign.end,
+                                      style: blackTextFont.copyWith(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(
+                                    "Tanggal & Waktu",
+                                    style: grayTextFont.copyWith(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                  Text(
+                                    ticket.time.dateAndTime,
+                                    textAlign: TextAlign.end,
+                                    style: blackTextFont.copyWith(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(
+                                    "Nomor Kursi",
+                                    style: grayTextFont.copyWith(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                  Text(
+                                    ticket.seatsInString,
+                                    textAlign: TextAlign.end,
+                                    style: blackTextFont.copyWith(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(
+                                    "ID Pembelian",
+                                    style: grayTextFont.copyWith(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                  Text(
+                                    ticket.bookingCode,
+                                    textAlign: TextAlign.end,
+                                    style: blackTextFont.copyWith(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
       ),
     );
   }
+}
+
+class TicketTopClipper extends CustomClipper<Path> {
+  double radius = 15;
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - radius);
+    path.quadraticBezierTo(radius, size.height - radius, radius, size.height);
+    path.lineTo(size.width - radius, size.height);
+    path.quadraticBezierTo(size.width - radius, size.height - radius,
+        size.width, size.height - radius);
+    path.lineTo(size.width, 0);
+
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class TicketBottomClipper extends CustomClipper<Path> {
+  double radius = 15;
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, radius);
+    path.quadraticBezierTo(size.width - radius, radius, size.width - radius, 0);
+    path.lineTo(radius, 0);
+    path.quadraticBezierTo(radius, radius, 0, radius);
+
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
