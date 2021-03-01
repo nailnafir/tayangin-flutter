0b49a005661866f19aecad20ef3b4f3a5b4cf616
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-01 19:22:43 +0700

    menambahkan keterangan user pada profile page

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -16,43 +16,103 @@ class _ProfilePageState extends State<ProfilePage> {
       child: Scaffold(
         body: Container(
           color: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
           child: ListView(
             children: <Widget>[
-              Column(
-                children: <Widget>[
-                  //NOTE: HEADER
-                  Container(
-                    margin: EdgeInsets.only(top: 16),
-                    child: Stack(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(1),
-                          alignment: Alignment.centerLeft,
-                          child: GestureDetector(
-                            onTap: () {
-                              context
-                                  .bloc<PageBloc>()
-                                  .add(GoToMainPage(bottomNavBarIndex: 0));
-                            },
-                            child: Icon(Icons.arrow_back, color: Colors.black),
-                          ),
-                        ),
-                        Center(
-                          child: Text(
-                            "Profil Saya",
-                            style: blackTextFont.copyWith(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
+              BlocBuilder<UserBloc, UserState>(builder: (_, userState) {
+                if (userState is UserLoaded) {
+                  UserApp user = userState.user;
+
+                  return Column(
+                    children: [
+                      Stack(
+                        children: <Widget>[
+                          //NOTE: HEADER
+                          Container(
+                            margin: EdgeInsets.only(top: 16),
+                            child: Stack(
+                              children: <Widget>[
+                                Container(
+                                  margin: EdgeInsets.only(left: defaultMargin),
+                                  padding: EdgeInsets.all(1),
+                                  alignment: Alignment.centerLeft,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      context.bloc<PageBloc>().add(
+                                          GoToMainPage(bottomNavBarIndex: 0));
+                                    },
+                                    child: Icon(Icons.arrow_back,
+                                        color: Colors.black),
+                                  ),
+                                ),
+                                Center(
+                                  child: Text(
+                                    "Profil Saya",
+                                    style: blackTextFont.copyWith(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Stack(),
-                ],
-              )
+                          //NOTE: PHOTO PROFILE
+                          Container(
+                            height: 350,
+                            margin: EdgeInsets.only(top: 120),
+                            decoration: BoxDecoration(
+                              color: mainColorBlue,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(50),
+                                topRight: Radius.circular(50),
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Center(
+                                child: Container(
+                                  width: 100,
+                                  height: 100,
+                                  margin: EdgeInsets.only(top: 70),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: (user.profilePicture != "")
+                                          ? NetworkImage(user.profilePicture)
+                                          : AssetImage(
+                                              "assets/user_pic_default.png"),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 12),
+                              Text(
+                                user.name,
+                                style: whiteTextFont.copyWith(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                user.email,
+                                style: whiteTextFont.copyWith(
+                                  color: accentColorBlue2,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                } else {
+                  return SizedBox();
+                }
+              })
             ],
           ),
         ),
