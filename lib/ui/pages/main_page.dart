4fb76123fb0c24ad6632b71654e478b1471d3499
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-02 17:11:30 +0700

    perbaikan button wallet mengarah ke top up page

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -62,8 +62,7 @@ class _MainPageState extends State<MainPage> {
                   ),
                 ),
                 onPressed: () {
-                  context.bloc<UserBloc>().add(SignOut());
-                  AuthServices.signOut();
+                  context.bloc<PageBloc>().add(GoToTopUpPage(GoToMainPage()));
                 },
               ),
             ),
