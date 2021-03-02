1902da47f4f8b3f069dbae2fda45bf57cf58504d
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-02 18:49:27 +0700

    membuat string extension pada extension

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -5,3 +5,4 @@ import 'package:tayangin_flutter/services/services.dart';
 
 part 'firebase_user_extension.dart';
 part 'date_time_extension.dart';
+part 'string_extension.dart';
--- lib/ui/pages/topup_page.dart
+++ lib/ui/pages/topup_page.dart
@@ -10,17 +10,76 @@ class TopUpPage extends StatefulWidget {
 }
 
 class _TopUpPageState extends State<TopUpPage> {
+  TextEditingController amountController = TextEditingController(text: 'IDR 0');
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: () async {
-          context.bloc<PageBloc>().add(widget.pageEvent);
-          return;
-        },
-        child: Scaffold(
-          body: Center(
-            child: Text("Isi Saldo"),
-          ),
-        ));
+      onWillPop: () async {
+        context.bloc<PageBloc>().add(widget.pageEvent);
+        return;
+      },
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            SafeArea(
+              child: Container(color: Colors.white),
+            ),
+            ListView(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                  child: Column(
+                    children: <Widget>[
+                      //NOTE: ICON BACK
+                      Container(
+                        margin: EdgeInsets.only(top: 16),
+                        child: Stack(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(1),
+                              alignment: Alignment.centerLeft,
+                              child: GestureDetector(
+                                onTap: () {
+                                  context
+                                      .bloc<PageBloc>()
+                                      .add(widget.pageEvent);
+                                },
+                                child:
+                                    Icon(Icons.arrow_back, color: Colors.black),
+                              ),
+                            ),
+                            Center(
+                              child: Text(
+                                "Isi Saldo",
+                                style: blackTextFont.copyWith(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      //NOTE: TEXTFIELD
+                      SizedBox(height: 30),
+                      TextField(
+                        controller: amountController,
+                        onChanged: (text) {},
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          labelText: "Jumlah",
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
