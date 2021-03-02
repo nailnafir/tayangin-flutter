a7c4ed2b321dc51563b2196199cbb557a5bd8827
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-02 16:54:00 +0700

    membuat top up page

---
+++ lib/ui/pages/topup_page.dart
@@ -0,0 +1,13 @@
+part of 'pages.dart';
+
+class TopUpPage extends StatefulWidget {
+  @override
+  _TopUpPageState createState() => _TopUpPageState();
+}
+
+class _TopUpPageState extends State<TopUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
