e8291fd95d791b89aec6b8260fb7c4037ee79e83
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-03 18:41:26 +0700

    menambahkan month name pada date time extension

--- lib/extensions/date_time_extension.dart
+++ lib/extensions/date_time_extension.dart
@@ -5,19 +5,19 @@ extension DateTimeExtension on DateTime {
   String get shortDayName {
     switch (this.weekday) {
       case 1:
-        return "Senin";
+        return "Sen";
       case 2:
-        return "Selasa";
+        return "Sel";
       case 3:
-        return "Rabu";
+        return "Rab";
       case 4:
-        return "Kamis";
+        return "Kam";
       case 5:
-        return "Jumat";
+        return "Jum";
       case 6:
-        return "Sabtu";
+        return "Sab";
       default:
-        return "Minggu";
+        return "Min";
     }
   }
 
@@ -66,6 +66,35 @@ extension DateTimeExtension on DateTime {
         return "Nov";
       default:
         return "Des";
+    }
+  }
+
+  String get monthName {
+    switch (this.month) {
+      case 1:
+        return "Januari";
+      case 2:
+        return "Februari";
+      case 3:
+        return "Maret";
+      case 4:
+        return "April";
+      case 5:
+        return "Mei";
+      case 6:
+        return "Juni";
+      case 7:
+        return "Juli";
+      case 8:
+        return "Agustus";
+      case 9:
+        return "September";
+      case 10:
+        return "Oktober";
+      case 11:
+        return "November";
+      default:
+        return "Dessember";
     }
   }
 
