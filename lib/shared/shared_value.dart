c670aa592a0944a51c19f5f1123248d5d43b0148
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-04 11:09:17 +0700

    menambahkan contact me url pada shared value

--- lib/shared/shared_value.dart
+++ lib/shared/shared_value.dart
@@ -2,6 +2,8 @@ part of 'shared.dart';
 
 String apiKey = "c8d8956be56524af43299f98c8a2677c";
 String imageBaseURL = "https://image.tmdb.org/t/p/";
+String contactMeURL =
+    "https://api.whatsapp.com/send?phone=6281288070110&text=Assalamu'alaikum,%20mas%20Firdaus.%20Saya%20dari%20aplikasi%20Tayangin...";
 
 PageEvent prevPageEvent;
 File imageFileToUpload;
