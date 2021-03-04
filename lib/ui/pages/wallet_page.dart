cb91b1c92c3cf97318a09e8c9c0ab57699dd2ef3
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-04 12:48:19 +0700

    membuat card clipper pada wallet page

--- lib/ui/pages/wallet_page.dart
+++ lib/ui/pages/wallet_page.dart
@@ -13,10 +13,104 @@ class WalletPage extends StatelessWidget {
         return;
       },
       child: Scaffold(
-        body: Center(
-          child: Text("Dompet Saya"),
+        body: Stack(
+          children: [
+            SafeArea(child: Container(color: Colors.white)),
+            ListView(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                  child: Column(
+                    children: <Widget>[
+                      //NOTE: ICON BACK
+                      Container(
+                        margin: EdgeInsets.only(top: 16),
+                        child: Stack(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(1),
+                              alignment: Alignment.centerLeft,
+                              child: GestureDetector(
+                                  onTap: () {
+                                    context.bloc<PageBloc>().add(pageEvent);
+                                  },
+                                  child: Icon(Icons.arrow_back,
+                                      color: Colors.black)),
+                            ),
+                            Center(
+                              child: Text(
+                                'Dompet Saya',
+                                style: blackTextFont.copyWith(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      //NOTE: CARD
+                      Container(
+                        height: 180,
+                        width: double.infinity,
+                        margin: EdgeInsets.symmetric(vertical: 30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: mainColorBlue,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 3.0,
+                                spreadRadius: 0.5,
+                                offset: Offset(0.0, 3.0))
+                          ],
+                        ),
+                        child: Stack(
+                          children: <Widget>[
+                            ClipPath(
+                              clipper: CardReflectionClipper(),
+                              child: Container(
+                                height: 180,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  gradient: LinearGradient(
+                                      begin: Alignment.bottomRight,
+                                      end: Alignment.topLeft,
+                                      colors: [
+                                        Colors.white.withOpacity(0.08),
+                                        Colors.white.withOpacity(0.08),
+                                      ]),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
   }
+}
+
+class CardReflectionClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height - 15);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
