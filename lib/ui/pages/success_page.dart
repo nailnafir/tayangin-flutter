cef50f3b9824531bf68815d3188518640c435f8b
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-04 18:36:52 +0700

    perbaikan fungsi button wallet page pada success page

--- lib/ui/pages/success_page.dart
+++ lib/ui/pages/success_page.dart
@@ -19,88 +19,89 @@ class SuccessPage extends StatelessWidget {
           future: ticket != null
               ? processingTicketOrder(context)
               : processingTopUp(context),
-          builder: (_, snapshot) => (snapshot.connectionState ==
-                  ConnectionState.done)
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 250,
-                      height: 250,
-                      margin: EdgeInsets.only(bottom: 80),
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage(ticket == null
-                                  ? "assets/illustration_success_top_up.png"
-                                  : "assets/illustration_success_buy.png"))),
-                    ),
-                    Text(
-                      ticket == null
-                          ? "Hmmm, Syukurlah!"
-                          : "Yuhuuu, Nonton Film!",
-                      style: blackTextFont.copyWith(
-                          fontSize: 20, fontWeight: FontWeight.w500),
-                    ),
-                    SizedBox(height: 18),
-                    Text(
-                      ticket == null
-                          ? "Sekarang saldo kamu\nbertambah banyak"
-                          : "Kamu telah berhasil\nmembeli tiket bioskop",
-                      style: grayTextFont.copyWith(
-                          fontSize: 14, fontWeight: FontWeight.w400),
-                    ),
-                    Container(
-                      width: 250,
-                      height: 50,
-                      margin: EdgeInsets.only(top: 50, bottom: 20),
-                      child: RaisedButton(
-                        elevation: 2,
-                        color: mainColorBlue,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        child: Text(
-                          ticket == null ? "Dompet Saya" : "Tiket Saya",
-                          style: whiteTextFont.copyWith(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
+          builder: (_, snapshot) =>
+              (snapshot.connectionState == ConnectionState.done)
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          width: 250,
+                          height: 250,
+                          margin: EdgeInsets.only(bottom: 80),
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(ticket == null
+                                      ? "assets/illustration_success_top_up.png"
+                                      : "assets/illustration_success_buy.png"))),
+                        ),
+                        Text(
+                          ticket == null
+                              ? "Hmmm, Syukurlah!"
+                              : "Yuhuuu, Nonton Film!",
+                          style: blackTextFont.copyWith(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(height: 18),
+                        Text(
+                          ticket == null
+                              ? "Sekarang saldo kamu\nbertambah banyak"
+                              : "Kamu telah berhasil\nmembeli tiket bioskop",
+                          style: grayTextFont.copyWith(
+                              fontSize: 14, fontWeight: FontWeight.w400),
+                        ),
+                        Container(
+                          width: 250,
+                          height: 50,
+                          margin: EdgeInsets.only(top: 50, bottom: 20),
+                          child: RaisedButton(
+                            elevation: 2,
+                            color: mainColorBlue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Text(
+                              ticket == null ? "Dompet Saya" : "Tiket Saya",
+                              style: whiteTextFont.copyWith(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            onPressed: () {
+                              if (ticket == null) {
+                                context.bloc<PageBloc>().add(GoToWalletPage(
+                                    GoToMainPage(bottomNavBarIndex: 0)));
+                              } else {
+                                context
+                                    .bloc<PageBloc>()
+                                    .add(GoToMainPage(bottomNavBarIndex: 1));
+                              }
+                            },
                           ),
                         ),
-                        onPressed: () {
-                          if (ticket == null) {
-                            context.bloc<PageBloc>().add(GoToWalletPage(null));
-                          } else {
-                            context
-                                .bloc<PageBloc>()
-                                .add(GoToMainPage(bottomNavBarIndex: 1));
-                          }
-                        },
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Mau nonton film lagi? ",
-                            style: grayTextFont.copyWith(
-                                fontWeight: FontWeight.w400)),
-                        GestureDetector(
-                          onTap: () {
-                            context.bloc<PageBloc>().add(GoToMainPage());
-                          },
-                          child: Text("Yuk lah!",
-                              style: yellowTextFont.copyWith(
-                                  fontWeight: FontWeight.w400)),
-                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text("Mau nonton film lagi? ",
+                                style: grayTextFont.copyWith(
+                                    fontWeight: FontWeight.w400)),
+                            GestureDetector(
+                              onTap: () {
+                                context.bloc<PageBloc>().add(GoToMainPage());
+                              },
+                              child: Text("Yuk lah!",
+                                  style: yellowTextFont.copyWith(
+                                      fontWeight: FontWeight.w400)),
+                            ),
+                          ],
+                        )
                       ],
                     )
-                  ],
-                )
-              : Center(
-                  child: SpinKitFadingCircle(
-                    size: 100,
-                    color: mainColorBlue,
-                  ),
-                ),
+                  : Center(
+                      child: SpinKitFadingCircle(
+                        size: 100,
+                        color: mainColorBlue,
+                      ),
+                    ),
         ),
       ),
     );
