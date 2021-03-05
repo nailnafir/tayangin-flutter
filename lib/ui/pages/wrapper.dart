f02ec9131f9f0a43359925ce1bbe57fbc7d2ee07
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-05 09:53:22 +0700

    menambahkan edit profile page pada wrapper

--- lib/ui/pages/wrapper.dart
+++ lib/ui/pages/wrapper.dart
@@ -71,13 +71,18 @@ class Wrapper extends StatelessWidget {
                                                                     pageState
                                                                         .pageEvent)
                                                                 : (pageState
-                                                                        is OnMainPage)
-                                                                    ? MainPage(
-                                                                        bottomNavBarIndex:
-                                                                            pageState.bottomNavBarIndex,
-                                                                        isExpired:
-                                                                            pageState.isExpired,
-                                                                      )
-                                                                    : Container());
+                                                                        is OnEditProfilePage)
+                                                                    ? EditProfilePage(
+                                                                        pageState
+                                                                            .user)
+                                                                    : (pageState
+                                                                            is OnMainPage)
+                                                                        ? MainPage(
+                                                                            bottomNavBarIndex:
+                                                                                pageState.bottomNavBarIndex,
+                                                                            isExpired:
+                                                                                pageState.isExpired,
+                                                                          )
+                                                                        : Container());
   }
 }
