1ce1db1be6e86a080c147585f132ca4f70af3513
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-07 07:50:02 +0700

    membuat forgot password page

---
+++ lib/ui/pages/forgot_password_page.dart
@@ -0,0 +1,8 @@
+part of 'pages.dart';
+
+class ForgotPasswordPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -30,6 +30,7 @@ part 'sign_in_page.dart';
 part 'sign_up_page.dart';
 part 'preference_page.dart';
 part 'account_confirmation_page.dart';
+part 'forgot_password_page.dart';
 part 'movie_page.dart';
 part 'wrapper.dart';
 part 'movie_detail_page.dart';
