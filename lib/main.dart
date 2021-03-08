3129c049c4d20fccfb61ba7dcffab0c7b820b3de
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-08 20:42:16 +0700

    update inisialisasi firebase core dan flutter binding pada main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
@@ -9,8 +10,10 @@ import 'package:tayangin_flutter/services/services.dart';
 import 'ui/pages/pages.dart';
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  // firebase versi baru harus inisialisasi dulu untuk dipanggil
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
