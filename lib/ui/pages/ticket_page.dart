61c394bb8b4613f91eed3ebe3c88c4098cd06323
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-13 12:47:09 +0700

    menambahkan fungsi refresh pada ticket page

--- lib/ui/pages/ticket_page.dart
+++ lib/ui/pages/ticket_page.dart
@@ -22,114 +22,121 @@ class _TicketPageState extends State<TicketPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          //NOTE: CONTENT
-          BlocBuilder<TicketBloc, TicketState>(
-            builder: (_, ticketState) => Container(
-              margin: EdgeInsets.symmetric(horizontal: defaultMargin),
-              child: TicketViewer(
-                isExpiredTickets
-                    ? ticketState.tickets
-                        .where((ticket) => ticket.time.isBefore(DateTime.now()))
-                        .toList()
-                    : ticketState.tickets
-                        .where(
-                            (ticket) => !ticket.time.isBefore(DateTime.now()))
-                        .toList(),
-              ),
-            ),
-          ),
-          //NOTE: HEADER
-          Container(height: 120, color: mainColorBlue),
-          SafeArea(
-            child: ClipPath(
-              clipper: HeaderClipper(),
-              child: Container(
-                height: 120,
-                color: mainColorBlue,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(bottom: 40),
-                      child: Text(
-                        "Tiket Saya",
-                        style: whiteTextFont.copyWith(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Column(
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  isExpiredTickets = !isExpiredTickets;
-                                });
-                              },
-                              child: Text(
-                                "Belum Tayang",
-                                style: whiteTextFont.copyWith(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                  color: !isExpiredTickets
-                                      ? Colors.white
-                                      : accentColorBlue2,
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 8),
-                            Container(
-                              height: 4,
-                              width: MediaQuery.of(context).size.width * 0.5,
-                              color: !isExpiredTickets
-                                  ? mainColorYellow
-                                  : Colors.transparent,
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  isExpiredTickets = !isExpiredTickets;
-                                });
-                              },
-                              child: Text(
-                                "Sudah Tayang",
-                                style: whiteTextFont.copyWith(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                  color: isExpiredTickets
-                                      ? Colors.white
-                                      : accentColorBlue2,
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 8),
-                            Container(
-                              height: 4,
-                              width: MediaQuery.of(context).size.width * 0.5,
-                              color: isExpiredTickets
-                                  ? mainColorYellow
-                                  : Colors.transparent,
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
+      body: RefreshIndicator(
+        color: mainColorBlue,
+        onRefresh: () async {
+          await Future.delayed(new Duration(seconds: 1));
+          setState(() {});
+        },
+        child: Stack(
+          children: <Widget>[
+            BlocBuilder<TicketBloc, TicketState>(
+              builder: (_, ticketState) => Container(
+                margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+                child: TicketViewer(
+                  isExpiredTickets
+                      ? ticketState.tickets
+                          .where(
+                              (ticket) => ticket.time.isBefore(DateTime.now()))
+                          .toList()
+                      : ticketState.tickets
+                          .where(
+                              (ticket) => !ticket.time.isBefore(DateTime.now()))
+                          .toList(),
                 ),
               ),
             ),
-          )
-        ],
+            //NOTE: HEADER
+            Container(height: 120, color: mainColorBlue),
+            SafeArea(
+              child: ClipPath(
+                clipper: HeaderClipper(),
+                child: Container(
+                  height: 120,
+                  color: mainColorBlue,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(bottom: 40),
+                        child: Text(
+                          "Tiket Saya",
+                          style: whiteTextFont.copyWith(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isExpiredTickets = !isExpiredTickets;
+                                  });
+                                },
+                                child: Text(
+                                  "Belum Tayang",
+                                  style: whiteTextFont.copyWith(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: !isExpiredTickets
+                                        ? Colors.white
+                                        : accentColorBlue2,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                              Container(
+                                height: 4,
+                                width: MediaQuery.of(context).size.width * 0.5,
+                                color: !isExpiredTickets
+                                    ? mainColorYellow
+                                    : Colors.transparent,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isExpiredTickets = !isExpiredTickets;
+                                  });
+                                },
+                                child: Text(
+                                  "Sudah Tayang",
+                                  style: whiteTextFont.copyWith(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: isExpiredTickets
+                                        ? Colors.white
+                                        : accentColorBlue2,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                              Container(
+                                height: 4,
+                                width: MediaQuery.of(context).size.width * 0.5,
+                                color: isExpiredTickets
+                                    ? mainColorYellow
+                                    : Colors.transparent,
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
