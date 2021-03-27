bd333b12c6b6f06ac0b0c9c21bbc5fb802716d3d
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-27 15:46:04 +0700

    menambahkan gesture detector on tap pada promo card

--- lib/ui/widgets/promo_card.dart
+++ lib/ui/widgets/promo_card.dart
@@ -8,124 +8,131 @@ class PromoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          height: 80,
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          decoration: BoxDecoration(
-            color: mainColorPrimary,
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 3.0,
-                  spreadRadius: 1.0,
-                  offset: Offset(0.0, 3.0))
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    promo.title,
-                    style: whiteTextFont.copyWith(
-                        fontSize: 14, fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    promo.subtitle,
-                    style: whiteTextFont.copyWith(
-                        fontSize: 12, fontWeight: FontWeight.w300),
-                  ),
-                ],
-              ),
-              Shimmer.fromColors(
-                baseColor: mainColorSecondary,
-                highlightColor: Colors.white,
-                child: Text(
-                  "DISC ${promo.discount}%",
-                  style: yellowNumberFont.copyWith(
-                      fontSize: 18, fontWeight: FontWeight.w600),
-                ),
-              )
-            ],
-          ),
-        ),
-        ShaderMask(
-          shaderCallback: (rectangle) {
-            return LinearGradient(
-                begin: Alignment.centerRight,
-                end: Alignment.centerLeft,
-                colors: [
-                  Colors.black.withOpacity(0.3),
-                  Colors.transparent,
-                ]).createShader(Rect.fromLTRB(0, 0, 105, 80));
-          },
-          blendMode: BlendMode.dstIn,
-          child: SizedBox(
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap();
+        }
+      },
+      child: Stack(
+        children: <Widget>[
+          Container(
             height: 80,
-            width: 105,
-            child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(8),
-                  bottomLeft: Radius.circular(8),
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              color: mainColorPrimary,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 3.0,
+                    spreadRadius: 1.0,
+                    offset: Offset(0.0, 3.0))
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      promo.title,
+                      style: whiteTextFont.copyWith(
+                          fontSize: 14, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      promo.subtitle,
+                      style: whiteTextFont.copyWith(
+                          fontSize: 12, fontWeight: FontWeight.w300),
+                    ),
+                  ],
                 ),
-                child: SvgPicture.asset("assets/reflection1.svg")),
+                Shimmer.fromColors(
+                  baseColor: mainColorSecondary,
+                  highlightColor: Colors.white,
+                  child: Text(
+                    "DISC ${promo.discount}%",
+                    style: yellowNumberFont.copyWith(
+                        fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: ShaderMask(
+          ShaderMask(
             shaderCallback: (rectangle) {
               return LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
+                  begin: Alignment.centerRight,
+                  end: Alignment.centerLeft,
                   colors: [
                     Colors.black.withOpacity(0.3),
                     Colors.transparent,
-                  ]).createShader(Rect.fromLTRB(0, 0, 155, 45));
+                  ]).createShader(Rect.fromLTRB(0, 0, 105, 80));
             },
             blendMode: BlendMode.dstIn,
             child: SizedBox(
-              height: 45,
-              width: 155,
+              height: 80,
+              width: 105,
               child: ClipRRect(
                   borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(8),
+                    topLeft: Radius.circular(8),
+                    bottomLeft: Radius.circular(8),
                   ),
-                  child: SvgPicture.asset("assets/reflection2.svg")),
+                  child: SvgPicture.asset("assets/reflection1.svg")),
             ),
           ),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: ShaderMask(
-            shaderCallback: (rectangle) {
-              return LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [
-                    Colors.black.withOpacity(0.35),
-                    Colors.transparent,
-                  ]).createShader(Rect.fromLTRB(0, 0, 135, 25));
-            },
-            blendMode: BlendMode.dstIn,
-            child: SizedBox(
-              height: 25,
-              width: 135,
-              child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(8),
-                  ),
-                  child: SvgPicture.asset("assets/reflection3.svg")),
+          Align(
+            alignment: Alignment.topRight,
+            child: ShaderMask(
+              shaderCallback: (rectangle) {
+                return LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Colors.black.withOpacity(0.3),
+                      Colors.transparent,
+                    ]).createShader(Rect.fromLTRB(0, 0, 155, 45));
+              },
+              blendMode: BlendMode.dstIn,
+              child: SizedBox(
+                height: 45,
+                width: 155,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(8),
+                    ),
+                    child: SvgPicture.asset("assets/reflection2.svg")),
+              ),
             ),
           ),
-        )
-      ],
+          Align(
+            alignment: Alignment.topRight,
+            child: ShaderMask(
+              shaderCallback: (rectangle) {
+                return LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Colors.black.withOpacity(0.35),
+                      Colors.transparent,
+                    ]).createShader(Rect.fromLTRB(0, 0, 135, 25));
+              },
+              blendMode: BlendMode.dstIn,
+              child: SizedBox(
+                height: 25,
+                width: 135,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(8),
+                    ),
+                    child: SvgPicture.asset("assets/reflection3.svg")),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
