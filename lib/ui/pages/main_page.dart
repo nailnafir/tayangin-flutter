2b9738984eb4e9a2ec7e4f25982fa351ec7a5b28
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-30 17:14:43 +0700

    migrasi context bloc ke context read pada main page

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -119,7 +119,7 @@ class _MainPageState extends State<MainPage> {
                     ),
                   ),
                   onPressed: () {
-                    context.bloc<PageBloc>().add(GoToTopUpPage(GoToMainPage()));
+                    context.read<PageBloc>().add(GoToTopUpPage(GoToMainPage()));
                   },
                 ),
               ),
