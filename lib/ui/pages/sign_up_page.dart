a0a921cadd138c225f4c7df0ca6964c4e0a5380d
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-31 09:07:34 +0700

    migrasi context bloc ke context read pada sign up page

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -31,12 +31,12 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     context
-        .bloc<ThemeBloc>()
+        .read<ThemeBloc>()
         .add(ChangeTheme(ThemeData().copyWith(primaryColor: mainColorPrimary)));
 
     return WillPopScope(
       onWillPop: () async {
-        context.bloc<PageBloc>().add(GoToSplashPage());
+        context.read<PageBloc>().add(GoToSplashPage());
         return;
       },
       child: Scaffold(
@@ -56,7 +56,7 @@ class _SignUpPageState extends State<SignUpPage> {
                           alignment: Alignment.centerLeft,
                           child: GestureDetector(
                             onTap: () {
-                              context.bloc<PageBloc>().add(GoToSplashPage());
+                              context.read<PageBloc>().add(GoToSplashPage());
                             },
                             child: Icon(Icons.arrow_back, color: Colors.black),
                           ),
@@ -253,7 +253,7 @@ class _SignUpPageState extends State<SignUpPage> {
                             widget.registrationData.password =
                                 passwordController.text;
 
-                            context.bloc<PageBloc>().add(
+                            context.read<PageBloc>().add(
                                 GoToPreferencePage(widget.registrationData));
                           }
                         },
