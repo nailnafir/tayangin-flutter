4c0ee27f5d035b2258e679801ba3d5597579c87e
Nailul Firdaus <nail.nafir@gmail.com>
2021-03-31 11:28:53 +0700

    migrasi raised button ke elevated button pada success page

--- lib/ui/pages/success_page.dart
+++ lib/ui/pages/success_page.dart
@@ -54,11 +54,13 @@ class SuccessPage extends StatelessWidget {
                           width: 250,
                           height: 50,
                           margin: EdgeInsets.only(top: 50, bottom: 12),
-                          child: RaisedButton(
-                            elevation: 2,
-                            color: mainColorPrimary,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8)),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              elevation: 2,
+                              primary: mainColorPrimary,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8)),
+                            ),
                             child: Text(
                               ticket == null ? "Dompet Saya" : "Tiket Saya",
                               style: whiteTextFont.copyWith(
