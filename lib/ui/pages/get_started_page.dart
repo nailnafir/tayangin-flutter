6bf8aefff22c55beb1cce7fd70e5a8b5a8bea078
Nailul Firdaus <nail.nafir@gmail.com>
2021-04-01 15:50:13 +0700

    mengubah nama class splash page ke get started page

--- lib/ui/pages/get_started_page.dart
+++ lib/ui/pages/get_started_page.dart
@@ -1,6 +1,6 @@
 part of 'pages.dart';
 
-class SplashPage extends StatelessWidget {
+class GetStartedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
