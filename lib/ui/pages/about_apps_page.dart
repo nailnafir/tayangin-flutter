ee9e8534c1a6d0f87df20f89e66000132743f642
Nailul Firdaus <nail.nafir@gmail.com>
2021-05-14 23:53:05 +0700

    mengubah title menjadi appbar pada about apps page

--- lib/ui/pages/about_apps_page.dart
+++ lib/ui/pages/about_apps_page.dart
@@ -9,98 +9,82 @@ class AboutAppsPage extends StatelessWidget {
         return;
       },
       child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Container(color: bgLight),
-            SafeArea(
-              child: Container(
-                color: bgLight,
-                padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        backgroundColor: bgLight,
+        appBar: AppBar(
+          leading: GestureDetector(
+            onTap: () {
+              context.read<PageBloc>().add(GoToProfilePage());
+            },
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+          ),
+          title: Text(
+            "Tentang Aplikasi",
+            style: whiteTextFont.copyWith(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              //NOTE: ILLUSTRATION
+              Container(
+                width: 300,
+                height: 300,
+                margin: EdgeInsets.only(bottom: 30),
+                child: Lottie.asset("assets/illustration_me.json"),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width - 2 * defaultMargin,
+                margin: EdgeInsets.symmetric(horizontal: defaultMargin),
                 child: Column(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(top: 16),
-                      child: Stack(
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(1),
-                            alignment: Alignment.centerLeft,
-                            child: GestureDetector(
-                              onTap: () {
-                                context.read<PageBloc>().add(GoToProfilePage());
-                              },
-                              child:
-                                  Icon(Icons.arrow_back, color: Colors.black),
-                            ),
-                          ),
-                          Center(
-                            child: Text(
-                              "Tentang Aplikasi",
-                              style: blackTextFont.copyWith(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          )
-                        ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Tayangin, aplikasi pembelian tiket bioskop secara online di Tangerang dan sekitarnya.",
+                      style: blackTextFont.copyWith(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
                       ),
+                      textAlign: TextAlign.justify,
                     ),
-                    //NOTE: ILLUSTRATION
-                    Container(
-                      width: 300,
-                      height: 300,
-                      margin: EdgeInsets.only(bottom: 30),
-                      child: Lottie.asset("assets/illustration_me.json"),
-                    ),
-                    Container(
-                      width:
-                          MediaQuery.of(context).size.width - 2 * defaultMargin,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Tayangin, aplikasi pembelian tiket bioskop secara online di Tangerang dan sekitarnya.",
-                            style: blackTextFont.copyWith(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                            ),
-                            textAlign: TextAlign.justify,
-                          ),
-                          SizedBox(height: 12),
-                          Text(
-                            "Coded with love by Nailul Firdaus.",
-                            style: blackTextFont.copyWith(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                            ),
-                            textAlign: TextAlign.justify,
-                          ),
-                          SizedBox(height: 12),
-                          Text(
-                            "Framework Flutter with Dart programming language and Firebase database by Google.",
-                            style: blackTextFont.copyWith(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                            ),
-                            textAlign: TextAlign.justify,
-                          ),
-                          SizedBox(height: 12),
-                          Text(
-                            "Versi 1.0.0\n${String.fromCharCode(169)}2020 - ${DateTime.now().year}. Tayangin.",
-                            style: grayTextFont.copyWith(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                            ),
-                            textAlign: TextAlign.justify,
-                          ),
-                        ],
+                    SizedBox(height: 12),
+                    Text(
+                      "Coded with love by Nailul Firdaus.",
+                      style: blackTextFont.copyWith(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
                       ),
-                    )
+                      textAlign: TextAlign.justify,
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      "Framework Flutter with Dart programming language and Firebase database by Google.",
+                      style: blackTextFont.copyWith(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      "Versi 1.0.0\n${String.fromCharCode(169)}2020 - ${DateTime.now().year}. Tayangin.",
+                      style: grayTextFont.copyWith(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
                   ],
                 ),
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
