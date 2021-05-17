ae7e4554f0edcc7b2c81a674961c91100af08a92
Nailul Firdaus <nail.nafir@gmail.com>
2021-05-17 07:40:54 +0700

    memperbaiki overflow pixel pada sign in page

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -39,167 +39,173 @@ class _SignInPageState extends State<SignInPage> {
       child: Scaffold(
         backgroundColor: bgLight,
         body: SafeArea(
-          child: Container(
-              margin: EdgeInsets.only(top: 30),
-              padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                      height: 80,
-                      width: 80,
-                      child: Image.asset("assets/logo_tayangin.png")),
-                  Container(
-                    margin: EdgeInsets.only(top: 50, bottom: 30),
-                    child: Text(
-                      "Selamat datang,\nTayangquwh!",
-                      style: blackTextFont.copyWith(fontSize: 20),
-                    ),
-                  ),
-                  TextField(
-                    onChanged: (text) {
-                      setState(() {
-                        isEmailValid = EmailValidator.validate(text);
-                      });
-                    },
-                    controller: emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        labelText: "Email Address"),
-                  ),
-                  SizedBox(height: 20),
-                  TextField(
-                    onChanged: (text) {
-                      setState(() {
-                        isPasswordValid = text.length >= 6;
-                      });
-                    },
-                    controller: passwordController,
-                    obscureText: !isPasswordVisible,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        suffixIcon: IconButton(
-                          icon: Icon(isPasswordVisible
-                              ? MdiIcons.eye
-                              : MdiIcons.eyeOff),
-                          onPressed: () {
-                            setState(() {
-                              isPasswordVisible = !isPasswordVisible;
-                            });
-                          },
-                        ),
-                        labelText: "Password"),
-                  ),
-                  SizedBox(height: 8),
-                  Row(
+          child: ListView(
+            children: [
+              Container(
+                  margin: EdgeInsets.only(top: 30),
+                  padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text("Lupa password? ",
-                          style: grayTextFont.copyWith(
-                              fontWeight: FontWeight.w400)),
-                      GestureDetector(
-                        onTap: () {
-                          context
-                              .read<PageBloc>()
-                              .add(GoToForgotPasswordPage());
-                        },
-                        child: Text("Reset disini",
-                            style: secondaryColorTextFont.copyWith(
-                                fontWeight: FontWeight.w400)),
-                      ),
-                    ],
-                  ),
-                  Center(
-                    child: Container(
-                      width: 250,
-                      height: 50,
-                      margin: EdgeInsets.only(top: 50, bottom: 12),
-                      child: isSigningIn
-                          ? SpinKitFadingCircle(color: mainColorPrimary)
-                          : FloatingActionButton(
-                              child: Text("Masuk",
-                                  style: isEmailValid && isPasswordValid
-                                      ? whiteTextFont.copyWith(fontSize: 16)
-                                      : grayTextFont.copyWith(fontSize: 16)),
-                              backgroundColor: isEmailValid && isPasswordValid
-                                  ? mainColorPrimary
-                                  : accentColorLightGray,
-                              elevation: 2,
-                              onPressed: isEmailValid && isPasswordValid
-                                  ? () async {
-                                      // ubah state jadi true
-                                      setState(() {
-                                        isSigningIn = true;
-                                      });
-
-                                      // tampung dan berikan result dari inputan text
-                                      SignInSignUpResult result =
-                                          await AuthServices.signIn(
-                                              emailController.text,
-                                              passwordController.text);
-
-                                      Fluttertoast.showToast(
-                                        msg: "Berhasil Masuk",
-                                        toastLength: Toast.LENGTH_SHORT,
-                                        gravity: ToastGravity.BOTTOM,
-                                      );
-
-                                      print(result.message ??
-                                          "SIGN IN STATUS: OK");
-
-                                      // ubah state lagi jadi false jika hasilnya keluar
-                                      if (result.user == null) {
-                                        setState(() {
-                                          isSigningIn = false;
-                                        });
-
-                                        // berikan pesan
-                                        Flushbar(
-                                          icon: Icon(
-                                              MdiIcons.closeCircleOutline,
-                                              color: Colors.white),
-                                          duration:
-                                              Duration(milliseconds: 3000),
-                                          flushbarPosition:
-                                              FlushbarPosition.TOP,
-                                          backgroundColor: mainColorSecondary,
-                                          message: result.message,
-                                        ).show(context);
-                                      }
-                                    }
-                                  : null,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8)),
-                            ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "Belum punya akun? ",
-                        style:
-                            grayTextFont.copyWith(fontWeight: FontWeight.w400),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          context
-                              .read<PageBloc>()
-                              .add(GoToRegistrationPage(RegistrationData()));
-                        },
+                      SizedBox(
+                          height: 80,
+                          width: 80,
+                          child: Image.asset("assets/logo_tayangin.png")),
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 30),
                         child: Text(
-                          "Buat disini",
-                          style: secondaryColorTextFont.copyWith(
-                              fontWeight: FontWeight.w400),
+                          "Selamat datang,\nTayangquwh!",
+                          style: blackTextFont.copyWith(fontSize: 20),
                         ),
+                      ),
+                      TextField(
+                        onChanged: (text) {
+                          setState(() {
+                            isEmailValid = EmailValidator.validate(text);
+                          });
+                        },
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                            labelText: "Email Address"),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        onChanged: (text) {
+                          setState(() {
+                            isPasswordValid = text.length >= 6;
+                          });
+                        },
+                        controller: passwordController,
+                        obscureText: !isPasswordVisible,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                            suffixIcon: IconButton(
+                              icon: Icon(isPasswordVisible
+                                  ? MdiIcons.eye
+                                  : MdiIcons.eyeOff),
+                              onPressed: () {
+                                setState(() {
+                                  isPasswordVisible = !isPasswordVisible;
+                                });
+                              },
+                            ),
+                            labelText: "Password"),
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        children: <Widget>[
+                          Text("Lupa password? ",
+                              style: grayTextFont.copyWith(
+                                  fontWeight: FontWeight.w400)),
+                          GestureDetector(
+                            onTap: () {
+                              context
+                                  .read<PageBloc>()
+                                  .add(GoToForgotPasswordPage());
+                            },
+                            child: Text("Reset disini",
+                                style: secondaryColorTextFont.copyWith(
+                                    fontWeight: FontWeight.w400)),
+                          ),
+                        ],
+                      ),
+                      Center(
+                        child: Container(
+                          width: 250,
+                          height: 50,
+                          margin: EdgeInsets.only(top: 50, bottom: 12),
+                          child: isSigningIn
+                              ? SpinKitFadingCircle(color: mainColorPrimary)
+                              : FloatingActionButton(
+                                  child: Text("Masuk",
+                                      style: isEmailValid && isPasswordValid
+                                          ? whiteTextFont.copyWith(fontSize: 16)
+                                          : grayTextFont.copyWith(
+                                              fontSize: 16)),
+                                  backgroundColor:
+                                      isEmailValid && isPasswordValid
+                                          ? mainColorPrimary
+                                          : accentColorLightGray,
+                                  elevation: 2,
+                                  onPressed: isEmailValid && isPasswordValid
+                                      ? () async {
+                                          // ubah state jadi true
+                                          setState(() {
+                                            isSigningIn = true;
+                                          });
+
+                                          // tampung dan berikan result dari inputan text
+                                          SignInSignUpResult result =
+                                              await AuthServices.signIn(
+                                                  emailController.text,
+                                                  passwordController.text);
+
+                                          Fluttertoast.showToast(
+                                            msg: "Berhasil Masuk",
+                                            toastLength: Toast.LENGTH_SHORT,
+                                            gravity: ToastGravity.BOTTOM,
+                                          );
+
+                                          print(result.message ??
+                                              "SIGN IN STATUS: OK");
+
+                                          // ubah state lagi jadi false jika hasilnya keluar
+                                          if (result.user == null) {
+                                            setState(() {
+                                              isSigningIn = false;
+                                            });
+
+                                            // berikan pesan
+                                            Flushbar(
+                                              icon: Icon(
+                                                  MdiIcons.closeCircleOutline,
+                                                  color: Colors.white),
+                                              duration:
+                                                  Duration(milliseconds: 3000),
+                                              flushbarPosition:
+                                                  FlushbarPosition.TOP,
+                                              backgroundColor:
+                                                  mainColorSecondary,
+                                              message: result.message,
+                                            ).show(context);
+                                          }
+                                        }
+                                      : null,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Belum punya akun? ",
+                            style: grayTextFont.copyWith(
+                                fontWeight: FontWeight.w400),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              context.read<PageBloc>().add(
+                                  GoToRegistrationPage(RegistrationData()));
+                            },
+                            child: Text(
+                              "Buat disini",
+                              style: secondaryColorTextFont.copyWith(
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
-              )),
+                  )),
+            ],
+          ),
         ),
       ),
     );
